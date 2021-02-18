9dbd6cfcdf2e30b0b4b69973dc945d7b16cec16f
Elvira A.Kulikova <e.kulikova@norsi-trans.ru>
2021-02-18 13:41:58 +0300

    using json instead csv

--- utils/database.py
+++ utils/database.py
@@ -1,44 +1,46 @@
-'''
-Concered with storing and retrieving books from a csv file.
-Format of the csv file:
+import json
 
-name,author,read\n
+'''
+Concered with storing and retrieving books from a json file.
+[
+    {
+        'name': 'Clean Code',
+        'author': 'Robert',
+        'read': True
+    }
+]
 '''
 
-books_file = 'books.txt'
+books_file = 'books.json'
+
 
 def create_book_table():
-    with open(books_file, 'w'):
-        pass # just to make the file is there
+    with open(books_file, 'w') as file:
+        json.dump([], file)
 
 
 def add_book(name, author):
-    with open(books_file, 'a') as file:
-        file.write(f'{name},{author},0\n')
+    books = get_all_books()
+    books.append({'name': name, 'author': author, 'read': False})
+    _save_all_books(books)
 
 
 def get_all_books():
     with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
+        return json.load(file)
 
-    return [
-        {'name': line[0], 'author': line[1], 'read': line[2]}
-        for line in lines
-    ]
+
+def _save_all_books(books):
+    with open(books_file, 'w') as file:
+        json.dump(books, file)
 
 
 def mark_book_as_read(name):
     books = get_all_books()
     for book in books:
         if book['name'] == name:
-            book['read'] = '1'
+            book['read'] = True
     _save_all_books(books)
-
-
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        for book in books:
-            file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
 
 def delete_book(name):
